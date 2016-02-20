846a462e3c659362cd029bfce6e09d3019f661f0
mvig <k580bm80@gmail.com>
2016-02-20 13:57:02 +0200

    Реализовал то же самое при помощи метода в классе Point

--- sendbox/src/main/java/lesson1/home02/Solution.java
+++ sendbox/src/main/java/lesson1/home02/Solution.java
@@ -9,23 +9,24 @@ import java.io.InputStreamReader;
  */
 public class Solution {
     public static void main(String[] args) throws IOException {
-        Point p1 = new Point();
-        Point p2 = new Point();
+
         BufferedReader inp = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.println("Введите координаты перовй точки в двумерной плоскости (x,y):");
-        p1.x = Integer.parseInt(inp.readLine());
-        p1.y = Integer.parseInt(inp.readLine());
+        int p1x = Integer.parseInt(inp.readLine());
+        int p1y = Integer.parseInt(inp.readLine());
+        Point p1 = new Point(p1x, p1y);
+
         System.out.println("Введите координаты второй точки в двумерной плоскости (x,y):");
-        p2.x = Integer.parseInt(inp.readLine());
-        p2.y = Integer.parseInt(inp.readLine());
+        int p2x = Integer.parseInt(inp.readLine());
+        int p2y = Integer.parseInt(inp.readLine());
+        Point p2 = new Point(p2x, p2y);
 
-        System.out.println(distance(p1, p2));
+        Point l = new Point(p1, p2);
+
+        System.out.println(l.distance(p1, p2));
 
 
     }
 
-    public static double distance(Point p1, Point p2) {
-
-        return Math.sqrt((p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y));
-    }
 }
