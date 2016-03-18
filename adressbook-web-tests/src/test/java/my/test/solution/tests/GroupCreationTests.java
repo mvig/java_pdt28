6c881856df767637b19368c6d9f148553a05a1ab
mvig <k580bm80@gmail.com>
2016-03-19 01:37:22 +0200

    Восстановление проделанной работы по группам после сбоя хранилища

--- adressbook-web-tests/src/test/java/my/test/solution/tests/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/my/test/solution/tests/GroupCreationTests.java
@@ -4,32 +4,24 @@ import my.test.solution.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
-
         app.getNavigationHelper().gotoGroupPage();
+        GroupData group = new GroupData( "test", "test1", "test2");
+        if (!app.getGroupHelper().isThereGroup()) app.getGroupHelper().createGroup(group);
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("test2", "test1", "test2");
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals(after.size(), before.size() + 1);
-        before.add(group);
-        int max = 0;
-        for (GroupData g : after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-
-
-        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
-        before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+         before.add(group);
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 
 }
