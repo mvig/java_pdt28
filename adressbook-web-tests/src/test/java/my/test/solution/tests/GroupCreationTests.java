2803f0a1db0f3e99b72049128f9824777401b7d7
mvig <k580bm80@gmail.com>
2016-03-19 10:26:07 +0200

    Группы ок

--- adressbook-web-tests/src/test/java/my/test/solution/tests/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/my/test/solution/tests/GroupCreationTests.java
@@ -12,12 +12,12 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
-        GroupData group = new GroupData( "test", "test1", "test2");
+        GroupData group = new GroupData( "test222", "test1", "test2");
         if (!app.getGroupHelper().isThereGroup()) app.getGroupHelper().createGroup(group);
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
-         before.add(group);
+        before.add(group);
         Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
         before.sort(byId);
         after.sort(byId);
