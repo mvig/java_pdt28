5e6e43c9933ddb6333c3b6d15a055f5b52c7242e
mvig <k580bm80@gmail.com>
2016-04-15 22:20:30 +0300

    Задание №19: Подготовить новый проект для нового учебного приложения
    Подготовить новый проект для нового учебного приложения:
    
    Создать новый модуль в IDEA
    Добавить в него конфигурационный файл Gradle и всё необходимое для
    запуска сборщика
    Создать структуру директорий для хранения кода.
    Создать TestBase и ApplicationManager (перенести из существующего модуля
    и адаптировать, удалить весь неиспользуемый код).
    
    ---

---
+++ mantis-tests/src/test/java/my/mantis/tests/TestBase.java
@@ -0,0 +1,29 @@
+package my.mantis.tests;
+
+import my.mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+
+
+/**
+ * Created by Tirex on 28.02.2016.
+ */
+public class TestBase {
+
+    //protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+
+}
